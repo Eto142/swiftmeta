1f42dbd1f18a93d3336fd9035f35cb3fe5650498
eto <eto.saviour80@gmail.com>
2025-11-29 21:27:01 +0000

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Mail;
 
 
 
@@ -53,9 +54,7 @@ class LoginController extends Controller
 //     }
 // }
 
-use Illuminate\Support\Facades\Mail;
-use App\Mail\OtpMail;
-use Illuminate\Support\Facades\Auth;
+
 
 public function login(Request $request)
 {
